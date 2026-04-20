e62ca9c56d4b7fe14e3b03048a3d6b74e8f5bb5d
awscloud2352 <poonamapotikar7@gmail.com>
2026-04-20 21:16:18 +0530

    Update main.tf
    properties VPC updated

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "terraform_test_vpc" {
   cidr_block           = "${var.vpc_cidr}"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_dns_hostnames = false
+  enable_dns_support   = false
 
   tags = {
     Name = "${var.cloud_env}_terraform_test_vpc"
