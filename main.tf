7644a87a873d1c739cf76a01f735d8e1ad895a0a
awscloud2352 <poonamapotikar7@gmail.com>
2026-04-20 21:19:43 +0530

    Update main.tf
    one is true vpc

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "terraform_test_vpc" {
   cidr_block           = "${var.vpc_cidr}"
-  enable_dns_hostnames = false
+  enable_dns_hostnames = True
   enable_dns_support   = false
 
   tags = {
